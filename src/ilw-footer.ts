adbe8c61e61f73a9d2af32508a3d369e0c1e6608
Andrew Reynolds <areynold@illinois.edu>
2024-12-17 11:14:47 -0600

    import il-footer code

--- src/ilw-footer.ts
+++ src/ilw-footer.ts
@@ -3,29 +3,108 @@ import { LitElement, html, unsafeCSS } from "lit";
 import styles from './ilw-footer.styles.css?inline';
 import './ilw-footer.css';
 
-class Footer extends LitElement {
-
-    static get properties() {
-        return {
-            theme: { type: String, attribute: true }
-        };
+export class Footer extends LitElement {
+    static properties = {
+        _data: { attribute: false }
     }
 
-    static get styles() {
-        return unsafeCSS(styles);
-    }
+    static styles = styles;
 
     constructor() {
         super();
-        this.theme = '';
+        this._data = undefined;
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        this.loadData();
+    }
+
+    async loadData() {
+        const resp = await fetch('https://cdn.brand.illinois.edu/data/footer.json');
+        this._data = await resp.json();
+    }
+
+    renderCampusSections() {
+        if (this._data === undefined) return;
+        const sections = this._data.illinois.sections.map(section => {
+            return html`<div class="section">
+          <h3>${section.label}</h3>
+          ${this.renderCampusLinks(section.links)}
+        </div>`
+        })
+        return html`<div class="sections">${sections}</div>`
+    }
+
+    renderCampusLinks(links) {
+        const listItems = links.map(link => {
+            return html`<li><a href="${link.href}">${link.label}</a></li>`
+        })
+        return html`<ul>${listItems}</ul>`;
+    }
+
+    renderCampusFooter() {
+        return html`
+        <div class="campus section-container">
+          <div class="campus section">
+            <h2 class="logo">
+              <a href="https://illinois.edu/">${wordmark}</a>
+            </h2>
+            ${this.renderCampusSections()}
+          </div>
+        </div>`;
+    }
+
+    renderLegalFooter() {
+        return html`
+        <div class="legal section-container">
+          <div class="legal section">
+            <div class="cookies-button-and-links">
+              <slot name="cookies-button"></slot>
+              <a href="https://www.vpaa.uillinois.edu/resources/web_privacy">Privacy</a></li>
+              <a href="https://illinois.edu/resources/website/copyright.html">Copyright</a></li>
+              <a href="https://illinois.edu/resources/website/accessibility.html">Accessibility</a></li>
+            </div>
+          </div>
+        </div>`;
+    }
+
+    renderSiteFooter() {
+        return html`
+        <div class="site section-container">
+          <div class="site section">
+            <div class="site-name">
+              <slot name="site-name"></slot>
+            </div>
+            <div class="actions">
+              <slot name="actions"></slot>
+            </div>
+            <div class="contact">
+              <div class="social">
+                <slot name="social"></slot>
+              </div>
+              <div class="address">
+                <slot name="address"></slot>
+              </div>
+              <div class="primary-unit">
+                <slot name="primary-unit"></slot>
+              </div>
+            </div>
+            <div class="content">
+              <slot></slot>
+            </div>
+          </div>
+        </div>`;
     }
 
     render() {
         return html`
-            <div>
-                <slot></slot>
-            </div>
-        `;
+        <footer class="footer">
+          ${this.renderSiteFooter()}
+          ${this.renderCampusFooter()}
+          ${this.renderLegalFooter()}
+        </footer>
+      `
     }
 }
 
