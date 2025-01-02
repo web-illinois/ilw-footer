d43e82694a3fa029e98a79a97b8ce245ad47160f
Andrew Reynolds <areynold@illinois.edu>
2025-01-02 10:05:53 -0600

    correct test sample filename

--- tests/footer.tests.js
+++ tests/footer.tests.js
@@ -1,10 +1,9 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('the footer contains a footer landmark', async ({page}) => {
-  await page.goto('samples/footer.html');
+test('the footer contains a footer landmark', async ({ page }) => {
+  await page.goto('samples/index.html');
   const val = await page.evaluate(() => {
     return document.querySelector('ilw-footer').shadowRoot.querySelector('footer');
   })
   await expect(val).not.toBeNull();
 })
-
