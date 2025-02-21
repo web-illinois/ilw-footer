75d817aa224d98a6166dfe338601e75a6cd4acbc
Andrew Reynolds <areynold@illinois.edu>
2025-02-21 11:21:17 -0600

    update build step

--- vite.transpile.config.ts
+++ vite.transpile.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     root: "src",
     build: {
-        outDir: "../dist/cdn",
+        outDir: "../dist",
         lib: {
             name: "ilw-footer",
             entry: "ilw-footer.ts",
@@ -12,9 +13,11 @@ export default defineConfig({
             formats: ["es"],
         },
         rollupOptions: {
+            external: [/^@?lit/],
             output: {
                 assetFileNames: (chunkInfo) => {
                     if (chunkInfo.name === "style.css") return "ilw-footer.css";
+                    return "assets/[name]-[hash][extname]"; // vite default
                 },
             },
         },
@@ -22,4 +25,5 @@ export default defineConfig({
     server: {
         hmr: false,
     },
+    plugins: [dts()],
 });
