63d29b7e1d3e3bc22588217aace6460031e92160
Andrew Reynolds <areynold@illinois.edu>
2025-07-21 15:00:21 -0500

    limit scan to footer

--- tests/accessibility.tests.js
+++ tests/accessibility.tests.js
@@ -18,11 +18,35 @@ test.describe('Accessibility Scan', () => {
 
     test('footer should pass all WCAG 2.1 AA automated scans', async ({ page }) => {
         await page.goto('tests/html/accessibility.html');
+        await page.waitForLoadState('domcontentloaded');
 
         const scanner = new AxeBuilder({ page })
             .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']);
 
-        const results = await scanner.analyze();
+        const results = await scanner.include({ fromShadowDom: ['footer'] }).analyze();
+        console.debug({ results });
+
+        // const scanner = new AxeBuilder({ page })
+        //     .options({
+        //         tags: [
+        //             'wcag2a',
+        //             'wcag2aa',
+        //             'wcag21a',
+        //             'wcag21aa'
+        //         ],
+        //         include: {
+        //             fromShadowDom: ['.footer']
+        //         },
+        //         rules: {
+        //             'color-contrast': { enabled: true }
+        //         }
+        //     });
+
+
+        // const results = await scanner.analyze();
+        // const contrast = results.passes.find(item => item.id === 'color-contrast');
+        // const items = contrast.nodes;
+        // console.debug({ items })
         const violations = results.violations;
 
         expect(violations).toEqual([]);
