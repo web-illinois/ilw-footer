4c85265c6743f5e7c6f575eafbb273ef28db0cca
Andrew Reynolds <areynold@illinois.edu>
2025-10-27 10:57:33 -0500

    update dev dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vitest/config";
+import { playwright } from "@vitest/browser-playwright"
 
 export default defineConfig({
     test: {
         browser: {
             enabled: true,
-            provider: "playwright",
+            provider: playwright(),
             // https://vitest.dev/guide/browser/playwright
             instances: [{ browser: "chromium" }, { browser: "firefox" }, { browser: "webkit" }],
         },
